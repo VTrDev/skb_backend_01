844c94bf28a1f47d726329a651cfff8c9dcc56c6
Vladimir Trusov <trusov.public@gmail.com>
2016-11-07 20:49:39 +0300

    Task 2B completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import _ from 'lodash';
+import XRegExp from 'xregexp';
 
 const app = express();
 app.use(cors());
@@ -21,7 +23,51 @@ app.get('/task2A', (req, res) => {
 /* =========================================== */
 
 
+/** 
+ * Задача 2B: Фамилия И. О.
+ */
+
+function trasformFullname(fullname) {
+  
+  const re = new XRegExp("^(\\p{L}|[ '])*$");
+  let result = 'Invalid fullname';
+
+  if (!re.test(fullname)) {    
+    return result;
+  }
+  
+  let fullname_parts = _.split(fullname , ' ');  
+  _.remove(fullname_parts, elem => elem === "");  
+  fullname_parts = fullname_parts.map(elem => _.capitalize(elem));    
+  
+  switch(fullname_parts.length) {
+    case 1: {
+      result = fullname_parts[0];
+      break;
+    }  
+    case 2: {
+      const [name, surname] = fullname_parts;
+      result = surname + ' ' + name.charAt(0) + '.';
+      break;
+    }        
+    case 3: {
+      const [name, patronymic, surname] = fullname_parts;
+      result =  surname + ' ' + name.charAt(0) + '. ' + patronymic.charAt(0) + '.';
+    } 
+  }
+
+  return result;
+}
+
+app.get('/task2B',  (req, res) => {   
+
+  res.send(trasformFullname(req.query.fullname));
+
+});
+
+/* =========================================== */
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
